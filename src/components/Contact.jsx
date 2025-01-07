5d27b4c50b58891c6179961d61d24d29dc5e10bc
Muthukumaran R <kumaranraja222@gmail.com>
2025-01-07 15:48:08 +0530

    message alert bug fix

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef, useState } from 'react'
+import React, { useRef, useState } from 'react'
 import "./Contact.css"
 import contactimg from '../assets/contact.png'
 import { FaHome } from "react-icons/fa";
