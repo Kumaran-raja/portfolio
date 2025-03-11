4de0000be8ba69b72c513210dcd6a2e101512b81
team 404 <team404errorfound@gmail.com>
2025-03-11 14:07:04 +0530

    bug fix

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import Certificates from './components/Certificate'
 import Skills from './components/Skills'
 import Projects from './components/Projects'
 import Contacts from './components/Contact'
-import { BrowserRouter as Router,Routes,Route } from 'react-router';
+import { BrowserRouter as Router,Routes,Route } from 'react-router-dom';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
@@ -16,12 +16,11 @@ root.render(
     <Header />
     <Routes>
       <Route path="/" element={<Home />}/>
-      <Route path="/Certificates" element={<Certificates />}/>
-      <Route path="/Skills" element={<Skills />}/>
-      <Route path="/Projects" element={<Projects />}/>
-      <Route path="/Contacts" element={<Contacts />}/>
+      <Route path="/certificates" element={<Certificates />}/>
+      <Route path="/skills" element={<Skills />}/>
+      <Route path="/projects" element={<Projects />}/>
+      <Route path="/contacts" element={<Contacts />}/>
     </Routes>
   </Router>
 );
-
 reportWebVitals();
